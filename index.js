795a17df74d6b5260170b37b4d1be360ca6618a6
FrimpongAlbertAttakora <kwaafjets2310@gmail.com>
2020-05-28 03:30:10 +0000

    Another commit

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ mongoose.connect(
        useUnifiedTopology: true, 
     },
     () => console.log('Connected to DB')
-);
+).catch(err => console.log(err));
 
 const port = process.env.PORT || 1800;
 app.listen(port, () => console.log(`server started on port ${port}`));
